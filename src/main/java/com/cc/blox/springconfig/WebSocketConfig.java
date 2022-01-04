a72f91334b74e0bcad0d1143c830755d8062f119
techy-Pie <techyPie.007@gmail.com>
2022-01-04 16:24:12 +0000

    Fix CORS issue

--- src/main/java/com/cc/blox/springconfig/WebSocketConfig.java
+++ src/main/java/com/cc/blox/springconfig/WebSocketConfig.java
@@ -21,7 +21,7 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 	
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/track").withSockJS();
+		registry.addEndpoint("/track").setAllowedOrigins("*:1108").withSockJS();
 	}
 	
 	@Override
