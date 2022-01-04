bbe4bbd35cb9fde044edcd77b7de702f6295b7d6
Sauldier001001 <saul.dierDev@gmail.com>
2022-01-04 16:29:49 +0000

    Add sessionid

--- src/main/java/com/cc/blox/springconfig/HttpHandshakeInterceptor.java
+++ src/main/java/com/cc/blox/springconfig/HttpHandshakeInterceptor.java
@@ -17,7 +17,11 @@ public class HttpHandshakeInterceptor implements HandshakeInterceptor {
 	@Override
 	public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler,
 			Map attributes) throws Exception {
-		
+		if (request instanceof ServletServerHttpRequest) {
+			ServletServerHttpRequest servletRequest = (ServletServerHttpRequest) request;
+			HttpSession session = servletRequest.getServletRequest().getSession();
+			attributes.put("sessionId", session.getId());
+		}		
 		return true;
 	}
 
